76bf2cccaabd877129c6cd4c6a02fdc0536f4b64
Mitraws <m.ws@outlook.com>
2020-07-01 15:37:03 +0200

    Wrap App.js w/ <Provider> and reorganize structure

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./components/HomeScreen";
-import StartGameScreen from "./components/StartGameScreen";
-import GameScreen from "./components/GameScreen";
-import ScoreScreen from "./components/ScoreScreen";
+import HomeScreen from "./src/Screens/HomeScreen";
+import StartGameScreen from "./src/Screens/StartGameScreen";
+import GameScreen from "./src/Screens/GameScreen";
+import ScoreScreen from "./src/Screens/ScoreScreen";
+import { Provider as StoreProvider } from "react-redux";
+import store from './src/store/index.js'
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="StartGame" component={StartGameScreen} />
-        <Stack.Screen name="Game" component={GameScreen} />
-        <Stack.Screen name="Score" component={ScoreScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StoreProvider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="StartGame" component={StartGameScreen} />
+          <Stack.Screen name="Game" component={GameScreen} />
+          <Stack.Screen name="Score" component={ScoreScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StoreProvider>
   );
 }
-
-
-
