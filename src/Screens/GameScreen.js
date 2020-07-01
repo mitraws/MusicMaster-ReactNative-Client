a6cac30781eef1ac21ad185aa8951bd22c0af718
Mitraws <m.ws@outlook.com>
2020-07-01 19:21:16 +0200

    Add swipe functionality

--- src/Screens/GameScreen.js
+++ src/Screens/GameScreen.js
@@ -1,10 +1,16 @@
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import randomcolor from "randomcolor";
 import { fetchData } from "../store/song/actions";
 import { useSelector, useDispatch } from "react-redux";
 import { selectLoading, selectData } from "../store/song/selectors";
 import Spinner from "../../assets/spinner/Spinner";
+import Swiper from "react-native-deck-swiper";
+
+const colors = {
+  white: "#ffffff",
+  black: "#000000",
+};
 
 export default function GameScreen({ navigation }) {
   const dispatch = useDispatch();
@@ -15,28 +21,59 @@ export default function GameScreen({ navigation }) {
     dispatch(fetchData);
   }, [dispatch]);
 
+  const Card = ({ card }) => {
+    return (
+      <View style={styles.card}>
+        {loading ? <Spinner /> : null}
+        <Text
+          style={{
+            fontWeight: "bold",
+            fontSize: 50,
+            textAlign: "center",
+          }}
+        >
+          {song.lyricSnippet}
+        </Text>
+        <Text
+          style={{
+            marginTop: 60,
+          }}
+        >
+          {song.artistName}
+        </Text>
+        <Text>{song.songName}</Text>
+      </View>
+    );
+  };
+
+
   return (
-    <View
-      onSwipeFromLeft={() => navigation.goBack()}
-      style={{
-        backgroundColor: randomcolor(),
-        alignItems: "center",
-        justifyContent: "center",
-        flex: 1,
-      }}
-    >
-      {loading ? <Spinner /> : null}
-      <Text
-        style={{
-          fontWeight: "bold",
-          fontSize: 30,
-          textAlign: "center",
-        }}
-      >
-        {song.lyricSnippet}
-      </Text>
-      <Text>{song.artistName}</Text>
-      <Text>{song.songName}</Text>
+    <View style={styles.Container}>
+      <Swiper
+        cards={song}
+        renderCard={(card) => <Card card={card} />}
+        infinite
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    flex: 0.9,
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: colors.black,
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 0 },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: randomcolor(),
+  },
+});
