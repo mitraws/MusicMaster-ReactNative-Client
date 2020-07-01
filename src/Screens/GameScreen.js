fa3c2e974a2dcfa5dafd0f41a405dcb96d793597
Mitraws <m.ws@outlook.com>
2020-07-01 19:57:24 +0200

    Event callback on swipe left to render new lyric

--- src/Screens/GameScreen.js
+++ src/Screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import randomcolor from "randomcolor";
 import { fetchData } from "../store/song/actions";
@@ -16,6 +16,7 @@ export default function GameScreen({ navigation }) {
   const dispatch = useDispatch();
   const loading = useSelector(selectLoading);
   const song = useSelector(selectData);
+  const [order, setOrder] = useState(0);
 
   useEffect(() => {
     dispatch(fetchData);
@@ -46,13 +47,20 @@ export default function GameScreen({ navigation }) {
     );
   };
 
+  const onSwipedLeft = () => {
+    dispatch(fetchData);
+    setOrder((order + 1) % song.length);
+    console.log("SWIPE FROM RIGHT?", order);
+  };
 
   return (
     <View style={styles.Container}>
       <Swiper
         cards={song}
+        cardOrder={order}
         renderCard={(card) => <Card card={card} />}
         infinite
+        onSwipedLeft={onSwipedLeft}
       />
     </View>
   );
