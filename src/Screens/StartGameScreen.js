3f7f82dd185b58d005ed675388acfa8fff5d76f6
Mitraws <m.ws@outlook.com>
2020-07-02 11:42:41 +0200

    Add inputfield to enter player on startGameScreen

--- src/Screens/StartGameScreen.js
+++ src/Screens/StartGameScreen.js
@@ -1,11 +1,19 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TextInput, Button } from "react-native";
+import randomcolor from "randomcolor";
 
 export default function StartGameScreen({ navigation }) {
+  const [player, setPlayer] = useState("");
+
   return (
     <View
       onSwipeFromLeft={() => navigation.goBack()}
-      style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+        backgroundColor: randomcolor(),
+      }}
     >
       <Text
         style={{
@@ -17,6 +25,23 @@ export default function StartGameScreen({ navigation }) {
       >
         Enter players:
       </Text>
+      <View style={{ flexDirection: "row" }}>
+        <TextInput
+          blurOnSubmit={true}
+          maxLength={20}
+          autoCapitalize="words"
+          style={{
+            backgroundColor: "white",
+            borderColor: randomcolor(),
+            borderWidth: 1,
+            padding: 9,
+          }}
+          placeholder="Player name"
+          onChangeText={(text) => setPlayer(text)}
+          value={player}
+        ></TextInput>
+        <Button title="Add" />  
+      </View>
     </View>
   );
 }
