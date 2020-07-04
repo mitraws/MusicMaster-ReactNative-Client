d2f6cde93a217207083a4893ee9bbba1bff5cf3d
Mitraws <m.ws@outlook.com>
2020-07-05 00:10:04 +0200

    Add animation, fade in text

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,12 +1,35 @@
-import React from "react";
-import { Text, View, Button } from "react-native";
+import React, { useRef } from "react";
+import { Animated, Text, View, Button } from "react-native";
 
 export default function HomeScreen({ navigation }) {
+  const FadeInView = (props) => {
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    React.useEffect(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 5000,
+        useNativeDriver: true,
+      }).start();
+    }, []);
+
+    return (
+      <Animated.View // Special animatable View
+        style={{
+          ...props.style,
+          opacity: fadeAnim, // Bind opacity to animated value
+        }}
+      >
+        {props.children}
+      </Animated.View>
+    );
+  };
+
   return (
     <View
       style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
     >
-      <View>
+      <FadeInView>
         <Text
           style={{
             fontWeight: "bold",
@@ -20,7 +43,7 @@ export default function HomeScreen({ navigation }) {
           title="Start Game"
           onPress={() => navigation.navigate("StartGame")}
         />
-      </View>
+      </FadeInView>
     </View>
   );
 }
